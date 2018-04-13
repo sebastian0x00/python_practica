b9d4e31300c7cf872158fc62fb9d09a854ccca41
sugusrojo <sugusrojo@gmail.com>
2018-04-13 15:19:33 -0300

    Create 23_riddling-kithmaze.py

---
+++ codecombat/kithgar/23_riddling-kithmaze.py
@@ -0,0 +1,8 @@
+# Loops son la mejor forma de hacer cosas repetitivamente
+
+while True:
+    # Añade sentencias aquí para ser repetidas.
+    hero.moveRight()
+    hero.moveDown()
+    hero.moveRight(2)
+    hero.moveUp()
