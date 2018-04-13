a498737152dc336e7731a3da4ef18981d6157be4
sugusrojo <sugusrojo@gmail.com>
2018-04-13 14:24:48 -0300

    Create 13_favorable-odds.py

---
+++ codecombat/kithgar/13_favorable-odds.py
@@ -0,0 +1,12 @@
+# Attack both ogres and grab the gem.
+hero.moveRight()
+hero.attack("Krug")
+hero.attack("Krug")
+hero.moveUp()
+hero.moveRight()
+hero.moveUp()
+hero.attack("Grump")
+hero.attack("Grump")
+hero.attack("Grump")
+hero.attack("Grump")
+hero.moveLeft(2)
