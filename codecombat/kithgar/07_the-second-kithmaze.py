c3316d2f5e951ac95b9d06a5410b77a8f055b7b5
sugusrojo <sugusrojo@gmail.com>
2018-04-13 14:03:39 -0300

    Create 07_the-second-kithmaze.py

---
+++ codecombat/kithgar/07_the-second-kithmaze.py
@@ -0,0 +1,8 @@
+# Utiliza un while-true loop para salir del laberinto!
+
+while True:
+    hero.moveRight()
+    # Type in 3 more move commands to finish the maze:
+    hero.moveUp()
+    hero.moveRight()
+    hero.moveDown()
