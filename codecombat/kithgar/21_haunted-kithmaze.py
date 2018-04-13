14c40c74eb2938dccd971efc35dde936bb3df8b5
sugusrojo <sugusrojo@gmail.com>
2018-04-13 15:02:34 -0300

    Create 21_haunted-kithmaze.py

---
+++ codecombat/kithgar/21_haunted-kithmaze.py
@@ -0,0 +1,8 @@
+# Los bucles son la mejor manera de hacer algo repetitivo.
+
+while True:
+    # Añadir comandos aquí para repetirlos.
+    hero.moveRight()
+    hero.moveRight()
+    hero.moveUp()
+    hero.moveUp()
