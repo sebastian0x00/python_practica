964d4affc3548ee1c1663c3331e6d8cbcf454e97
sugusrojo <sugusrojo@gmail.com>
2018-04-13 16:47:23 -0300

    Create 24_signs-and-portents.py

---
+++ codecombat/kithgar/24_signs-and-portents.py
@@ -0,0 +1,11 @@
+# Tu objetivo es llegar vivo a la parte derecha del mapa.
+# No necesitas luchar con los ogros, solo ¡muévete! Tus aliados te protegerán.
+hero.moveRight()
+hero.moveRight()
+hero.moveUp()
+# Use the movement commands to reach the red X!
+hero.moveRight(3)
+hero.moveDown()
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
