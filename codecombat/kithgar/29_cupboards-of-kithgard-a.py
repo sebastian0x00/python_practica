33d5242b13aa905d390f5c927182ec167752906f
sugusrojo <sugusrojo@gmail.com>
2018-04-13 17:24:09 -0300

    Create 29_cupboards-of-kithgard-a.py

---
+++ codecombat/kithgar/29_cupboards-of-kithgard-a.py
@@ -0,0 +1,9 @@
+# Puede que haya algo por ahi para ayudarte
+
+# Primero, acércate al Armario.
+hero.moveDown()
+hero.moveLeft(2)
+hero.moveUp(2)
+# Luego, ataca el Armario dentro de un bucle.
+while True:
+    hero.attack("Cupboard")
