8e83cea255ad25ab990e277ff2be6d5795ae1458
sugusrojo <sugusrojo@gmail.com>
2018-04-13 13:50:43 -0300

    Update 04_true-names.py

--- codecombat/kithgar/04_true-names.py
+++ codecombat/kithgar/04_true-names.py
@@ -1,6 +1,12 @@
-# ¡Defiéndete de Brak y Treg!
 # Debes atacar a los pequeños ogros dos veces.
 
 hero.moveRight()
 hero.attack("Brak")
 hero.attack("Brak")
+hero.attack("Brak")
+hero.attack("Brak")
+hero.attack("Treg")
+hero.attack("Treg")
+hero.attack("Treg")
+hero.attack("Treg")
+hero.moveRight()
