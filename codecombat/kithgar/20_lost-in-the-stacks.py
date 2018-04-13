dedb91939ba59a9f89ec05c00cbc68bb0495825f
sugusrojo <sugusrojo@gmail.com>
2018-04-13 14:59:36 -0300

    Create 20_lost-in-the-stacks.py

---
+++ codecombat/kithgar/20_lost-in-the-stacks.py
@@ -0,0 +1,11 @@
+# El bucle repite un bloque de código de forma continuada.
+# Un bloque de codigo está tabulado con 4 espacios.
+
+# Un bloque de código comienza con dos puntos:
+while True:
+    # Las líneas de moviniento necesitan estar tabuladas con 4 espacios!
+    # Añade 4 espacios al comienzo de cada linea, usando el tabulador, bajo el comienzo del bucle while True 
+    hero.moveRight()
+    hero.moveUp() # ∆ INDENT this line
+    hero.moveLeft() # ∆ INDENT this line
+    hero.moveDown() # ∆ INDENT this line
