6a14536480384b2577aaf92dad2eb0300862c95c
sugusrojo <sugusrojo@gmail.com>
2018-04-13 16:53:03 -0300

    Create 27_known-enemy.py

---
+++ codecombat/kithgar/27_known-enemy.py
@@ -0,0 +1,12 @@
+# Se puede usar una variable como un apodo.
+
+enemy1 = "Kratt"
+enemy2 = "Gert"
+enemy3 = "Ursa"
+
+hero.attack(enemy1)
+hero.attack(enemy1)
+hero.attack(enemy2)
+hero.attack(enemy2)
+hero.attack(enemy3)
+hero.attack(enemy3)
