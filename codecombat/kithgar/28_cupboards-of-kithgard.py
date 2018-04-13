93a728637dcb034d461e4f3860eaf61f74a7a46a
sugusrojo <sugusrojo@gmail.com>
2018-04-13 17:10:24 -0300

    Create 28_cupboards-of-kithgard.py

---
+++ codecombat/kithgar/28_cupboards-of-kithgard.py
@@ -0,0 +1,9 @@
+# ¡Puede que haya algo por ahí para ayudarte!
+
+# Primero, acércate a la Alacena.
+hero.moveUp()
+hero.moveRight(2)
+hero.moveDown(2)
+# Luego, ataca la Alacena dentro de un bucle 'while-true'.
+while True:
+    hero.attack("Cupboard")
