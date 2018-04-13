aca8c78c9838241db36113337709c8886c2bda50
sugusrojo <sugusrojo@gmail.com>
2018-04-13 14:34:56 -0300

    Create 14_sleeping-on-the-job.py

---
+++ codecombat/kithgar/14_sleeping-on-the-job.py
@@ -0,0 +1,15 @@
+# Los guardias protegen los botones de la puerta. 
+
+# Avanza entre los ogros que están durmiendo:
+hero.moveUp()
+hero.moveRight(2)
+# Derrota a los ogros atacándolos dos veces:
+hero.attack("Vera")
+hero.attack("Vera")
+hero.attack("Vera")
+hero.attack("Vera")
+hero.attack("Bas")
+hero.attack("Bas")
+hero.attack("Bas")
+# Llega hasta la gema y escapa:
+hero.moveRight()
