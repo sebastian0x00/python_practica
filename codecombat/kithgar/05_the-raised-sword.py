70e40e9dbba0720cc9349047aebbd81e0d3f3809
sugusrojo <sugusrojo@gmail.com>
2018-04-13 13:55:27 -0300

    Create 05_the-raised-sword.py

---
+++ codecombat/kithgar/05_the-raised-sword.py
@@ -0,0 +1,14 @@
+# Derrota a los ogros.
+# Recuerda que cada uno de ellos necesita dos golpes.
+hero.attack("Ack")
+hero.attack("Ack")
+hero.attack("Ack")
+hero.attack("Ack")
+hero.attack("Rig")
+hero.attack("Rig")
+hero.attack("Rig")
+hero.attack("Rig")
+hero.attack("Gurt")
+hero.attack("Gurt")
+hero.attack("Gurt")
+hero.attack("Gurt")
