b30c46cc2715e004f76a683639154ca5e140183d
sugusrojo <sugusrojo@gmail.com>
2018-04-13 14:58:44 -0300

    Create 19_loop-da-loop.py

---
+++ codecombat/kithgar/19_loop-da-loop.py
@@ -0,0 +1,11 @@
+# ¡El código que esté dentro de un bucle se repetirá eternamente!
+
+while True:
+    # Moverse a la derecha
+    hero.moveRight()
+    # Moverse arriba
+    hero.moveUp()
+    # Moverse a la izquierda
+    hero.moveLeft()
+    # Moverse abajo
+    hero.moveDown()
