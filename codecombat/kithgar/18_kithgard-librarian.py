8be3a88de42deba36bbd4fb317e1da72ad8fd2d4
sugusrojo <sugusrojo@gmail.com>
2018-04-13 14:56:20 -0300

    Create 18_kithgard-librarian.py

---
+++ codecombat/kithgar/18_kithgard-librarian.py
@@ -0,0 +1,8 @@
+# ¡Necesitas una contraseña para abrir la puerta de la biblioteca!
+# ¡La contraseña se encuentra en la guía de Ayuda!
+# Haz clic en el botón azul de Ayuda debajo de la ventana del código para abrir la guía de Ayuda del nivel.
+# La mayoría de niveles tienen una guía de Ayuda con información extra. Si te encuentras atascado en un nivel, asegúrate de hacer clic en la Ayuda!
+
+hero.moveRight()
+hero.say("Hush")  # ∆
+hero.moveRight()
