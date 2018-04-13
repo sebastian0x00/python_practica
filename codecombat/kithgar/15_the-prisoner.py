1d26a259b4dc21d6b2d516c6773ff430aaf7f699
sugusrojo <sugusrojo@gmail.com>
2018-04-13 14:43:10 -0300

    Create 15_the-prisoner.py

---
+++ codecombat/kithgar/15_the-prisoner.py
@@ -0,0 +1,14 @@
+# Free Patrick from behind the "Weak Door".
+hero.moveRight()
+hero.attack("Weak Door")
+hero.attack("Weak Door")
+hero.attack("Weak Door")
+hero.moveRight(3)
+# Defeat the guard, named "Two".
+hero.attack("Two")
+hero.attack("Two")
+hero.attack("Two")
+hero.attack("Two")
+# Get the gem.
+hero.moveDown()
+hero.moveRight()
