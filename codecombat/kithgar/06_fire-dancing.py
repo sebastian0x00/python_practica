f09cab1546321b797147cffa80c171dc82e18f6b
sugusrojo <sugusrojo@gmail.com>
2018-04-13 13:56:51 -0300

    Create 06_fire-dancing.py

---
+++ codecombat/kithgar/06_fire-dancing.py
@@ -0,0 +1,8 @@
+# El código se suele ejecutar en el orden escrito
+# los búcles te permiten repetir un bloque de código repetidas veces
+# Necesitar presionar tabulat (o poner 4 espacios) para tabular los movimientos bajo el búcle
+
+while True:
+    hero.moveRight()
+    # estos comentario abajo muestran cómo hacer un bucle
+    hero.moveLeft()
