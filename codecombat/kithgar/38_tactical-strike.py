a0d559c86962e4865b3000483e76c35c784c4896
sugusrojo <sugusrojo@gmail.com>
2018-04-14 18:46:53 -0300

    Create 38_tactical-strike.py

---
+++ codecombat/kithgar/38_tactical-strike.py
@@ -0,0 +1,10 @@
+# Defeat the ogres.
+hero.moveDown()
+hero.moveRight()
+enemy1 = hero.findNearestEnemy()
+hero.attack(enemy1)
+hero.attack(enemy1)
+enemy2 = hero.findNearestEnemy()
+hero.attack(enemy2)
+hero.attack(enemy2)
+hero.moveDown()
