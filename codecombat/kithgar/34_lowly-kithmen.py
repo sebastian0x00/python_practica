15f84b245898e8d66e6b0d6e090251d6a22cd7d0
sugusrojo <sugusrojo@gmail.com>
2018-04-14 18:12:06 -0300

    Create 34_lowly-kithmen.py

---
+++ codecombat/kithgar/34_lowly-kithmen.py
@@ -0,0 +1,13 @@
+# ¡Crear una segunda variable y ataca!
+
+enemy1 = hero.findNearestEnemy()
+hero.attack(enemy1)
+hero.attack(enemy1)
+
+enemy2 = hero.findNearestEnemy()
+hero.attack(enemy2)
+hero.attack(enemy2)
+
+hero.moveRight()
+hero.moveDown()
+hero.moveRight()
