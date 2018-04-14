caf343bad6cc73dca98400608e559540f5aae9d0
sugusrojo <sugusrojo@gmail.com>
2018-04-14 19:45:37 -0300

    Create 41_kithgard-gates.py

---
+++ codecombat/kithgar/41_kithgard-gates.py
@@ -0,0 +1,7 @@
+# ¡Construye 3 vallas para mantener a los ogros acorralados!
+
+hero.moveDown()
+hero.buildXY("fence", 36, 34)
+hero.buildXY("fence", 36, 31)
+hero.buildXY("fence", 36, 27)
+hero.moveRight(3)
