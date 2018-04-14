babf80d965905ef5d4c0db3d340754116eb13cd7
sugusrojo <sugusrojo@gmail.com>
2018-04-14 18:25:34 -0300

    Create 37_by-any-other-name.py

---
+++ codecombat/kithgar/37_by-any-other-name.py
@@ -0,0 +1,15 @@
+# Change the names of variables to make gems appear!
+# Change the name of variable "enemy1" to "chunky".
+chunky = hero.findNearestEnemy()
+hero.attack(chunky)
+hero.attack(chunky)
+
+# Change the name of variable "enemy2" to "monkey".
+monkey = hero.findNearestEnemy()
+hero.attack(monkey)
+hero.attack(monkey)
+
+# Collect the gems after changing the variables.
+hero.moveUp()
+hero.moveRight()
+hero.moveRight()
