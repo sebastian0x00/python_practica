d2e47fee3110605af25e463c3d4be0fb931cc524
sugusrojo <sugusrojo@gmail.com>
2018-04-14 22:59:22 -0300

    Create 01_over-the-garden-wall.py

---
+++ codecombat/campaign-game-dev-1/01_over-the-garden-wall.py
@@ -0,0 +1,18 @@
+# Usa el comando game.spawnXY() para construir una valla alrededor de la granja
+
+game.spawnXY("fence", 39, 16)
+# Construye 2 o mas vallas  a 4 metros de distancia
+# Coloca una valla en y 20
+game.spawnXY("fence", 39, 16)
+# Coloca una valla en y 24
+game.spawnXY("fence", 39, 24)
+game.spawnXY("fence", 39, 28)
+game.spawnXY("fence", 39, 32)
+# Construye 2 o mas vallas  a 4 metros de distancia
+# Coloca una valla en y 36
+game.spawnXY("fence", 39, 36)
+# Coloca una valla en y 40
+game.spawnXY("fence", 39, 40)
+game.spawnXY("fence", 39, 44)
+
+# When your fences are ready, press Test Level.
