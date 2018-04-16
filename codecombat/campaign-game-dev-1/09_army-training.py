e12422a079e0ba51c09d7ee48685156409c3398c
sugusrojo <sugusrojo@gmail.com>
2018-04-16 12:24:15 -0300

    Create 09_army-training.py

---
+++ codecombat/campaign-game-dev-1/09_army-training.py
@@ -0,0 +1,16 @@
+# Spawn a hero and add a goal.
+game.spawnHeroXY("champion", 40, 15)
+game.addDefeatGoal()
+
+# Spawn at least 2 "munchkin"s.
+game.spawnXY("munchkin", 36, 30)
+game.spawnXY("munchkin", 20, 30)
+# Spawn at least 2 "thrower"s.
+game.spawnXY("thrower", 20, 10) # Ogre Spear Thrower
+game.spawnXY("thrower", 20, 15) # Ogre Spear Thrower
+# Spawn at least 2 "soldier"s.
+game.spawnXY("soldier", 40, 25) # Human Soldier
+game.spawnXY("soldier", 40, 30) # Human Soldier
+# Spawn at least 2 "archer"s.
+game.spawnXY("archer", 10, 10)  # Human Archer
+game.spawnXY("archer", 40, 20)  # Human Archer
