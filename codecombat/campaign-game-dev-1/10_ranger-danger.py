8f0ee9b968bfa53e0f43412110372b922b990a0f
sugusrojo <sugusrojo@gmail.com>
2018-04-16 13:53:51 -0300

    Create 10_ranger-danger.py

---
+++ codecombat/campaign-game-dev-1/10_ranger-danger.py
@@ -0,0 +1,10 @@
+# Archers are powerful ranged units!
+# While soldiers have health, archers provide damage.
+
+# Too many soldiers, not enough archers!
+game.spawnXY("soldier", 20, 40)
+
+# Add 2 more archers:
+game.spawnXY("archer", 30, 30)
+game.spawnXY("archer", 30, 20)
+game.spawnXY("archer", 30, 10)
