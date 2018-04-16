94e7a5c356dd07011fccdc9ae9cd601c5651421f
sugusrojo <sugusrojo@gmail.com>
2018-04-16 14:24:23 -0300

    Create 11_hedge-magic.py

---
+++ codecombat/campaign-game-dev-1/11_hedge-magic.py
@@ -0,0 +1,7 @@
+# Genera un laberinto. Cambia el número para generar un laberinto diferente!
+game.spawnMaze(5)
+
+# Genera un héroe con  spawnHeroXY(type, x, y)
+hero = game.spawnHeroXY("captain", 12, 12)
+# Añade al menos un objetivo!
+game.addSurviveGoal(5)
