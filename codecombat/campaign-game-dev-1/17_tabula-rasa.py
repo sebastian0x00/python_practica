9cd606ee13dda005363afafa46ace0c5d40c6e3e
sugusrojo <sugusrojo@gmail.com>
2018-04-16 15:34:39 -0300

    Create 17_tabula-rasa.py

---
+++ codecombat/campaign-game-dev-1/17_tabula-rasa.py
@@ -0,0 +1,8 @@
+# ¡Crea tu propio juego!
+
+# Genera un héroe con spawnHeroXY(type, x, y)
+game.spawnHeroXY("captain", 24, 37)
+# ¡Añade como mínimo un objetivo!
+game.addSurviveGoal(5)
+# Genera objetos en el juego con spawnXY(type, x, y)
+game.spawnXY("gem", 21, 20)
