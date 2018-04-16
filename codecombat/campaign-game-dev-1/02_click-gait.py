aaf05f963e7e01a0a7897376ae93816041ff81b1
sugusrojo <sugusrojo@gmail.com>
2018-04-16 11:54:41 -0300

    Create 02_click-gait.py

---
+++ codecombat/campaign-game-dev-1/02_click-gait.py
@@ -0,0 +1,7 @@
+# No cambies ningún código. Simplemente haga clic en Test Level
+# Muevete a todas las marcas X rojas para ganar
+
+game.spawnHeroXY("knight", 5, 8)
+game.addMoveGoalXY(10, 18)
+game.addMoveGoalXY(10, 38)
+game.addMoveGoalXY(48, 41)
