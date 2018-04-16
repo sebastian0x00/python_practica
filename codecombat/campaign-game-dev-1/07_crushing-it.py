7ca082523d34453d078265fc5fa5ca65137880c3
sugusrojo <sugusrojo@gmail.com>
2018-04-16 12:17:58 -0300

    Create 07_crushing-it.py

---
+++ codecombat/campaign-game-dev-1/07_crushing-it.py
@@ -0,0 +1,14 @@
+# Si olvidas los comandos, mira debajo de la ventana de código!
+
+# Genera un héroe usando el comando  game.spawnHeroXY(type, x, y).
+game.spawnHeroXY("captain", 36, 30)
+game.addSurviveGoal()
+# Añade un objetivo de derrotar ogros con game.addDefeatGoal()
+game.addDefeatGoal()
+
+game.spawnXY("munchkin", 40, 10)
+# Genera al menos 3 o más munchkins.
+game.spawnXY("munchkin", 53, 22)
+game.spawnXY("munchkin", 20, 22)
+game.spawnXY("munchkin", 20, 30)
+game.spawnXY("munchkin", 20, 40)
