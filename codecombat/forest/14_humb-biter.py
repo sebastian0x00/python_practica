0010e9a0e9783755c572f361b2b2119bcecf9972
sugusrojo <sugusrojo@gmail.com>
2018-04-17 16:48:31 -0300

    Create 14_humb-biter.py

---
+++ codecombat/forest/14_humb-biter.py
@@ -0,0 +1,18 @@
+# If-statement code only runs when the if’s condition is true.
+# In a condition, == means "is equal to."
+if 2 + 2 == 4:
+    hero.say("Hey!")
+if 2 + 2 == 5:
+    hero.say("Yes, you!")
+
+# Change the condition here to make your hero say Come at me!
+if 3 + 3 == 6:  # ∆ Make this true.
+    hero.say("Come at me!")
+if 3 + 3 == 7:
+    hero.say("Cualquiera!")
+if 2 + 18 == 20:  # ∆ Make this true.
+    # Add one more taunt to lure the ogre. Be creative!
+    hero.say("ATR")
+if 2 + 19 == 20:
+    hero.say("AL TOKE PERRO")
+    pass
