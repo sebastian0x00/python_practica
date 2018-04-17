06274733a71d9cb8e72cbc1cdfc0d58cdbd3d4b8
sugusrojo <sugusrojo@gmail.com>
2018-04-17 13:40:06 -0300

    Create 03_winding-trail.py

---
+++ codecombat/forest/03_winding-trail.py
@@ -0,0 +1,10 @@
+# Ve al final del camino y construye una Fence(valla) allí.
+# Usa tu funcion moveXY(x, y)
+
+# It's the first point of the path.
+hero.moveXY(36, 59)
+# Move at the next points of the path.
+hero.moveXY(37, 13)
+# Build a fence to stop the ogre.
+hero.buildXY("fence", 72, 25)
+hero.moveXY(72, 25)
