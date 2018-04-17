bb9d21e1b1d72870631a591a92e9e7346b3ed7b5
sugusrojo <sugusrojo@gmail.com>
2018-04-17 13:37:51 -0300

    Create 02_defense-of-plainswood.py

---
+++ codecombat/forest/02_defense-of-plainswood.py
@@ -0,0 +1,4 @@
+# Construir dos fences(vallas) para mantener los aldeanos a salvo!
+# Coloca tu mouse sobre el mundo para obtener las coordenadas X,Y
+hero.buildXY("fence", 40, 52);
+hero.buildXY("fence", 40, 19);
