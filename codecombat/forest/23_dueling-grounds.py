82691062decd046ea6893d066f8f35294b1ae209
sugusrojo <sugusrojo@gmail.com>
2018-04-18 12:24:33 -0300

    Create 23_dueling-grounds.py

---
+++ codecombat/forest/23_dueling-grounds.py
@@ -0,0 +1,7 @@
+# Defeat the enemy hero in a duel!
+enemy = hero.findNearestEnemy()
+
+while True:
+    # Find and attack the enemy inside a loop.
+    # When you're done, submit to the multiplayer ladder!
+    hero.attack(enemy)
