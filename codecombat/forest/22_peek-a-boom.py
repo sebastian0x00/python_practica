92a52ae0aa15b208fe2a99d528c26ce90dd0d816
sugusrojo <sugusrojo@gmail.com>
2018-04-18 12:19:55 -0300

    Create 22_peek-a-boom.py

---
+++ codecombat/forest/22_peek-a-boom.py
@@ -0,0 +1,12 @@
+# Build traps on the path when the hero sees a munchkin!
+
+while True:
+    enemy = hero.findNearestEnemy()
+    if enemy:
+        # Build a "fire-trap" at the Red X (41, 24)
+        hero.buildXY("fire-trap", 41, 24)
+        pass
+    # Add an else below to move back to the clearing
+    else:
+        # Move to the Wooden X (19, 19)
+        hero.moveXY(19, 19)
