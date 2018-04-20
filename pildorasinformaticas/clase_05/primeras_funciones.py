0141237d88cfe120eb193f354cae85dd934c2bca
sugusrojo <sugusrojo@gmail.com>
2018-04-20 13:15:25 -0300

    Create primeras_funciones.py

---
+++ pildorasinformaticas/clase_05/primeras_funciones.py
@@ -0,0 +1,20 @@
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Poco a poco iremos avanzando")
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Poco a poco iremos avanzando")
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Poco a poco iremos avanzando")
+
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Poco a poco iremos avanzando")
+
+print("Estamos aprendiendo Python")
+print("Estamos aprendiendo instrucciones básicas")
+print("Poco a poco iremos avanzando")
