27aa31fbe346fdd6129e123047660336b880f5c0
sugusrojo <sugusrojo@gmail.com>
2018-04-20 13:16:17 -0300

    Create primeras_funciones_02.py

---
+++ pildorasinformaticas/clase_05/primeras_funciones_02.py
@@ -0,0 +1,14 @@
+def mensaje():
+
+	print("Estamos aprendiendo Python")
+	print("Estamos aprendiendo instrucciones básicas")
+	print("Poco a poco iremos avanzando")
+
+mensaje()
+
+
+mensaje()
+
+print("Ejecutando código fuera de función")
+
+mensaje()
