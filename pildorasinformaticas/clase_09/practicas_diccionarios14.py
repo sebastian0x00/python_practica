4e46762b6075c2ec095bde59f10e4911cd3165f6
sugusrojo <sugusrojo@gmail.com>
2018-05-08 12:53:10 -0300

    Create practicas_diccionarios14.py

---
+++ pildorasinformaticas/clase_09/practicas_diccionarios14.py
@@ -0,0 +1,4 @@
+midiccionario = {23: "Jordan", "Nombre": "Michael" , "Equipo" : "Chicago", "anillos" : {"temporadas":[1991,1992,1993,1996,1997,1998]}}
+print(midiccionario.keys())
+print(midiccionario.values())
+print(midiccionario)
