5c9f5e06eddaac731f241dd997771d7084477417
sugusrojo <sugusrojo@gmail.com>
2018-05-08 12:42:03 -0300

    Update practicas_diccionarios07.py

--- pildorasinformaticas/clase_09/practicas_diccionarios07.py
+++ pildorasinformaticas/clase_09/practicas_diccionarios07.py
@@ -1,3 +1,3 @@
 milista = ["España", "Francia", "Reino Unido" , "Alemania"]
 midiccionario = {milista[0]: "Madrid", milista[1]: "Paris", milista[2]:"Londres", milista[3]:"Berlin"}
-print(midiccionario[Francia])
+print(midiccionario["Francia"])
