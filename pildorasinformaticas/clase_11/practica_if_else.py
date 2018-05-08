cf6fc93861cdecdfd84ea9cdcbfb77e0b637230c
sugusrojo <sugusrojo@gmail.com>
2018-05-08 15:55:35 -0300

    Create practica_if_else.py

---
+++ pildorasinformaticas/clase_11/practica_if_else.py
@@ -0,0 +1,7 @@
+print("Verificación de acceso")
+edad_usuario = int(input("Introduce tu edad, por favor"))
+if edad_usuario < 18:
+    print("No puede pasar")
+else:
+    print("Puedes pasar")
+print("El programa ha finalizado")
