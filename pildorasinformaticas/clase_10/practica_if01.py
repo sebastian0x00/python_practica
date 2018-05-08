2a2a6035e6613f11ef56601ef267146c02a4e2fc
sugusrojo <sugusrojo@gmail.com>
2018-05-08 15:29:42 -0300

    Create practica_if01.py

---
+++ pildorasinformaticas/clase_10/practica_if01.py
@@ -0,0 +1,11 @@
+print("Programa de evaluación de notas de alumnos")
+
+nota_alumno=input()
+
+def evaluacion(nota):
+    valoracion = "aprobado"
+    if nota < 5:
+        valoracion = "suspenso"
+    return valoracion
+
+print(evaluacion(nota_alumno))
