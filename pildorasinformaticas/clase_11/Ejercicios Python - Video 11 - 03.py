5b680da1557ff4dd3927562d7233d7dfd48ed75e
sugusrojo <sugusrojo@gmail.com>
2018-05-08 16:43:50 -0300

    Create Ejercicios Python - Video 11 - 03.py

---
+++ pildorasinformaticas/clase_11/Ejercicios Python - Video 11 - 03.py
@@ -0,0 +1,12 @@
+#Crea un programa que pida tres numeros por teclado. El programa imprime en consola la media aritmética de los numeros introducidos
+
+num1 = int(input("Introduce el primer número: "))
+
+num2 = int(input("Introduce el segundo número: "))
+
+num3 = int(input("Introduce el tercer número: "))
+
+media = (num1 + num2 + num3) / 3
+
+print("La media aritmética es: ")
+print(media)
