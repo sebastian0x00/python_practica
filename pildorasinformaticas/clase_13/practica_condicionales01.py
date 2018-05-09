db0b4b6ed08228cb0771a1e545fa69eef8eeee99
sugusrojo <sugusrojo@gmail.com>
2018-05-09 15:56:53 -0300

    Create practica_condicionales01.py

---
+++ pildorasinformaticas/clase_13/practica_condicionales01.py
@@ -0,0 +1,8 @@
+print("Asignaturasoptativas año 2017")
+print("Asignaturas optativas: Informática gráfica - Pruebas de software - Usabilidad y accesibilidad")
+asignatura=input("Escribe la asignatura escogida: ")
+
+if asignatura in ("Informática gráfica",  "Pruebas de software" , "Usabilidad y accesibilidad"):
+	print ("Asignatura elegida " + asignatura)
+else:
+	print("La asigntura escogida no está cotemplada")
