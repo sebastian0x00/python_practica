7d432a035d006865811d024f780bfcc22a9fc3e5
sugusrojo <sugusrojo@gmail.com>
2018-05-10 16:01:01 -0300

    Create practica_for06.py

---
+++ pildorasinformaticas/clase_15/practica_for06.py
@@ -0,0 +1,12 @@
+contador = 0
+miEmail=input("Introduce tu dirreción de email: ")
+
+for i in miEmail:
+	if(i=="@" or i=="."):
+		contador=contador+1
+
+
+if contador==2:
+	print("Email es correcto")
+else:
+	print("El email no es correcto")
