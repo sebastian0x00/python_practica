71792ecbd5327cfcf0c6976a16d9a4b08859e442
sugusrojo <sugusrojo@gmail.com>
2018-05-11 17:43:48 -0300

    Create Ejercicios Python - Video 16 - 01.py

---
+++ pildorasinformaticas/clase_16/Ejercicios Python - Video 16 - 01.py
@@ -0,0 +1,5 @@
+#Ejericio 1
+#Crea un programa que muestre los números impares del 1 al 100. Los números deberán aparecer una al lado del otro sin salto de línea
+
+for i in range(1,100,2):
+	print(i, end=" ")
