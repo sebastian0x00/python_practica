b8686b3341902ff2bbaf44de9e9ed2652bee381d
sugusrojo <sugusrojo@gmail.com>
2018-05-17 16:02:00 -0300

    Create prueba_excepciones303.py

---
+++ pildorasinformaticas/clase_23/prueba_excepciones303.py
@@ -0,0 +1,20 @@
+import math
+
+def calculaRaiz(num1):
+    if num1 < 0:
+        raise ValueError ("El numero no puede ser negativo")
+
+    else:
+        return math.sqrt(num1)
+
+
+op1 = (int(input("Introduce un número: ")))
+
+try:
+    print(calculaRaiz(op1))
+
+except ValueError as ErrorDeNumeroNegativo:
+
+    print(ErrorDeNumeroNegativo)
+
+print("Programa terminado")
