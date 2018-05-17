8eaee99960a324b2aa0c6be46beab8a24f262610
sugusrojo <sugusrojo@gmail.com>
2018-05-17 15:39:07 -0300

    Update prueba_excepciones301.py

--- pildorasinformaticas/clase_23/prueba_excepciones301.py
+++ pildorasinformaticas/clase_23/prueba_excepciones301.py
@@ -1,1 +1,16 @@
 
+def evaluaedad(edad):
+
+    if edad < 0:
+        raise TypeError("No se permite edades negativas")
+
+    if edad < 20:
+        return "eres muy joven "
+    elif edad < 40:
+        return "Eres joven"
+    elif edad < 65:
+        return "Eres maduro"
+    elif edad < 100:
+        return "Cuídate..."
+
+print(evaluaedad(-15))
