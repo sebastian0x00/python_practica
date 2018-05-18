8f5a390661e8a6036625f78d5c668af6130ea1e9
sugusrojo <sugusrojo@gmail.com>
2018-05-18 12:39:05 -0300

    Create po_1.py

---
+++ pildorasinformaticas/clase_26/po_1.py
@@ -0,0 +1,24 @@
+class Coche():
+    #propiedades del tipo de coche
+    largoChasis = 250
+    anchoChasis = 250
+    ruedas = 4
+    enmarcha = False
+
+    def arrancar(self):   #comportamiento, arrancar es el metodo
+        self.enmarcha = True
+
+    def estado(self):
+        if(self.enmarcha):
+            return "El coche está en marcha"
+
+        else:
+            return "El coche está parado"
+
+miCoche = Coche() #instanciar una clase
+
+print("El largo del coche es: ", miCoche.largoChasis)
+print("El coche tiene ", miCoche.ruedas, "ruedas")
+miCoche.arrancar()
+
+print(miCoche.estado())
