78f2ba03481035d696fad380cdcdbd09542a60bc
sugusrojo <sugusrojo@gmail.com>
2018-05-22 16:09:09 -0300

    Create poo_03.py

---
+++ pildorasinformaticas/clase_27/poo_03.py
@@ -0,0 +1,39 @@
+class Coche():
+    #propiedades del tipo de coche
+    largoChasis = 250
+    anchoChasis = 250
+    ruedas = 4
+    enmarcha = False
+
+    def arrancar(self,arrancamos):
+        self.enmarcha=arrancamos
+
+        if(self.enmarcha):
+            return "El coche está en marcha"
+
+        else:
+            return "El coche está parado"
+
+
+    def estado(self):
+        print("El coche tiene ", self.ruedas, "ruedas. Un acho de ", self.anchoChasis, " y un largo de ",
+            self.largoChasis)
+
+
+
+miCoche=Coche()
+
+print("El largo del coche es: ", miCoche.largoChasis)
+print("El coche tiene ", miCoche.ruedas, "ruedas")
+print(miCoche.arrancar(True))
+
+miCoche.estado()
+
+print("---------------A continuación creamos el segundo objeto-------------")
+
+miCoche2=Coche()
+
+print("El largo del coche es: ", miCoche2.largoChasis)
+print("El coche tiene ", miCoche2.ruedas, "ruedas")
+print(miCoche2.arrancar(False))
+miCoche2.estado()
