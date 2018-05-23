345a5fc2694f997675d96c752418360ec9a6a42f
sugusrojo <sugusrojo@gmail.com>
2018-05-23 14:50:58 -0300

    Create poo_07.py

---
+++ pildorasinformaticas/clase_27/poo_07.py
@@ -0,0 +1,39 @@
+class Coche():
+
+    def __init__(self):
+        self.largoChasis = 250
+        self.anchoChasis = 250
+        self.__ruedas = 4
+        self.enmarcha = False
+
+    def arrancar(self,arrancamos):
+        self.enmarcha=arrancamos
+
+        if(self.enmarcha):
+            return "El coche está en marcha"
+
+        else:
+            return "El coche está parado"
+
+
+    def estado(self):
+        print("El coche tiene ", self.__ruedas, "ruedas. Un acho de ", self.anchoChasis, " y un largo de ",
+            self.largoChasis)
+
+
+
+miCoche=Coche()
+
+print(miCoche.arrancar(True))
+
+miCoche.estado()
+
+print("---------------A continuación creamos el segundo objeto-------------")
+
+miCoche2=Coche()
+
+print(miCoche2.arrancar(False))
+
+miCoche2.__ruedas = 2
+
+miCoche2.estado()
