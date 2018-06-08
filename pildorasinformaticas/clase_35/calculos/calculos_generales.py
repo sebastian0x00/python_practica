b4de45f76fc663c7300373847e6ce210aa05893e
sugusrojo <sugusrojo@gmail.com>
2018-06-08 16:07:01 -0300

    Create calculos_generales.py

---
+++ pildorasinformaticas/clase_35/calculos/calculos_generales.py
@@ -0,0 +1,17 @@
+def sumar(op1, op2):
+	print("El resultado de la suma es: ", op1 + op2)
+
+def restar(op1, op2):
+	print("El resultado de la suma es: ", op1 - op2)
+
+def multiplicar(op1, op2):
+	print("El resultado de la suma es: ", op1 * op2)
+
+def dividir(dividendo, divisor):
+	print("El resultado de la suma es: ", dividendo / divisor)
+
+def potencia(base, exponente):
+	print("El resultado de la suma es: ", base ** exponente)
+
+def redondear(numero):
+	print("El resultado de la suma es: ", round(numero))
