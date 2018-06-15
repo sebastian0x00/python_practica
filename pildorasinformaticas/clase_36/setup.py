7f9690639f2e55046629c75f5eebf83d67b8bb4b
sugusrojo <sugusrojo@gmail.com>
2018-06-15 16:26:01 -0300

    Create setup.py

---
+++ pildorasinformaticas/clase_36/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+
+	name="paquetecalculos",
+	version="1.0",
+	description="Paquete de redondeo y potencia",
+	author="Juan",
+	author_email="cursos@pildorasinformaticas.es",
+	url="www.pildorasinformaticas.es",
+	packages=["calculos","calculos.redondeo_potencia"]
+	
+	)
