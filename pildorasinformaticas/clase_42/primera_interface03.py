d781f9f303a9416c6dc44236405549eecc0505f9
sugusrojo <sugusrojo@gmail.com>
2018-08-01 15:24:30 -0300

    Create primera_interface03.py

---
+++ pildorasinformaticas/clase_42/primera_interface03.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+raiz = Tk()
+
+raiz.title("Ventana de pruebas")
+
+raiz.resizable(True, False) #width = ancho || height = alto -- 1 es igual a True, 0 a False
+
+raiz.mainloop()
