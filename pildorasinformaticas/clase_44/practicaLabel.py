d3ab41e479deb499d25e0374f39b1b36651d5f08
sugusrojo <sugusrojo@gmail.com>
2018-08-01 17:25:56 -0300

    Create practicaLabel.py

---
+++ pildorasinformaticas/clase_44/practicaLabel.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+miFrame = Frame(root, width = 500, height = 400)
+
+miFrame.pack()
+
+miLabel = Label(miFrame, text = "Hola alumnos de Python")
+
+miLabel.pack()
+
+root.mainloop()
