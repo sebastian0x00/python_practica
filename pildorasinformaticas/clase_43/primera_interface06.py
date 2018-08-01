98f4c2f8fc0b6dd8b8064e146627e9a942fa0549
sugusrojo <sugusrojo@gmail.com>
2018-08-01 16:19:52 -0300

    Create primera_interface06.py

---
+++ pildorasinformaticas/clase_43/primera_interface06.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+raiz = Tk()
+
+raiz.title("Ventana de pruebas")
+
+#raiz.resizable(True, False) 
+
+raiz.iconbitmap("mate.ico")
+
+#raiz.geometry("650x350")
+
+raiz.config(bg = "blue")
+
+miFrame = Frame()
+
+miFrame.pack(side = "left", anchor = "n")
+
+miFrame.config(bg = "red")
+
+miFrame.config(width = "650", height = "350")
+
+raiz.mainloop()
