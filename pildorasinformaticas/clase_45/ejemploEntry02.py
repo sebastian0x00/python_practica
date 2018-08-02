b41f8297557dd8fe52f6f2e28c3f888fc400a3bd
sugusrojo <sugusrojo@gmail.com>
2018-08-02 13:48:19 -0300

    Create ejemploEntry02.py

---
+++ pildorasinformaticas/clase_45/ejemploEntry02.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+raiz = Tk()
+
+miFrame = Frame(raiz, width = 1200, height = 600)
+miFrame.pack()
+cuadroTexto = Entry(miFrame)
+cuadroTexto.place(x = 100, y = 100)
+
+
+
+raiz.mainloop()
