23f36cec1d7e0652d4803b0252515bc20cd1ce5a
sugusrojo <sugusrojo@gmail.com>
2018-08-02 14:03:03 -0300

    Create ejemploEntry04.py

---
+++ pildorasinformaticas/clase_45/ejemploEntry04.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+raiz = Tk()
+
+miFrame = Frame(raiz, width = 1200, height = 600)
+miFrame.pack()
+
+cuadroNombre = Entry(miFrame)
+cuadroNombre.grid(row = 0, column = 1)
+
+cuadroApellido = Entry(miFrame)
+cuadroApellido.grid(row = 1, column = 1)
+
+cuadroDirrecion = Entry(miFrame)
+cuadroDirrecion.grid(row = 2  , column = 1)
+
+nombreLabel = Label(miFrame, text = "Nombre:")
+nombreLabel.grid(row = 0, column = 0) 
+
+apellidoLabel = Label(miFrame, text = "Apellido:")
+apellidoLabel.grid(row = 1, column = 0) 
+
+DirrecionLabel = Label(miFrame, text = "Dirreción de casa:")
+DirrecionLabel.grid(row = 2, column = 0) 
+
+raiz.mainloop()
