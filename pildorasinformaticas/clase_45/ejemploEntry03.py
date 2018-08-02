e9f1a53ad86f1c5c437fb95e78604a89415d2bd4
sugusrojo <sugusrojo@gmail.com>
2018-08-02 13:50:47 -0300

    Create ejemploEntry03.py

---
+++ pildorasinformaticas/clase_45/ejemploEntry03.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+raiz = Tk()
+
+miFrame = Frame(raiz, width = 1200, height = 600)
+miFrame.pack()
+cuadroTexto = Entry(miFrame)
+cuadroTexto.place(x = 100, y = 100)
+
+nombreLabel = Label(miFrame, text = "Nombre:")
+nombreLabel.place(x = 100, y = 100)
+
+raiz.mainloop()
