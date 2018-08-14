ba491d0ff344dac0cc5f5de23f87f1d899bfb171
Sebastián Cortés <sugusrojo@gmail.com>
2018-08-14 17:37:03 -0300

    Create practicaMenu06.py

---
+++ pildorasinformaticas/clase_52/practicaMenu06.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+root = Tk()
+
+barraMenu = Menu(root)
+root.config(menu = barraMenu, width = 300, height = 300)
+
+archivoMenu = Menu(barraMenu, tearoff = 0)
+
+archivoMenu.add_command(label = "Nuevo")
+
+archivoMenu.add_command(label = "Guardar")
+
+archivoMenu.add_command(label = "Guardar como")
+
+archivoMenu.add_command(label = "Cerrar")
+
+archivoMenu.add_command(label = "Salir")
+
+archivoEdicion = Menu(barraMenu)
+
+archivoHerramientas = Menu(barraMenu)
+
+archivoAyuda = Menu(barraMenu)
+
+barraMenu.add_cascade(label = "archivo", menu = archivoMenu)
+
+barraMenu.add_cascade(label = "Edición", menu = archivoEdicion)
+
+barraMenu.add_cascade(label = "Herramientas", menu = archivoHerramientas)
+
+barraMenu.add_cascade(label = "Ayuda", menu = archivoAyuda)
+
+root.mainloop()
