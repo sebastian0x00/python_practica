0921cba339492733f5323bf233dbf554a4be0e92
Sebastián Cortés <sugusrojo@gmail.com>
2018-08-14 17:31:52 -0300

    Create practicaMenu03.py

---
+++ pildorasinformaticas/clase_52/practicaMenu03.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+root = Tk()
+
+barraMenu = Menu(root)
+root.config(menu = barraMenu)
+
+archivoMenu = Menu(barraMenu)
+
+archivoEdicion = Menu(barraMenu)
+
+archivoHerramientas = Menu(barraMenu)
+
+archivoAyuda = Menu(barraMenu)
+
+barraMenu.add_cascade(label = "archivo", menu = archivoMenu)
+
+barraMenu.add_cascade(label = "Edición", menu = archivoEdicion)
+
+barraMenu.add_cascade(label = "Herramientas", menu = archivoHerramientas)
+
+barraMenu.add_cascade(label = "Ayuda", menu = archivoAyuda)
+
+root.mainloop()
