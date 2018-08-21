568fb1be16d745aba2ec2a3bacd47c4b66bc2438
Sebastián Cortés <sugusrojo@gmail.com>
2018-08-21 16:21:22 -0300

    Create Ejemplo1BBDD.py

---
+++ pildorasinformaticas/clase_56/Ejemplo1BBDD.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+miConexion = sqlite3.connect("PrimeraBase")
+
+miCursor = miConexion.cursor()
+
+#miCursor.execute("CREATE TABLE PRODUCTOS (NOMBRE_ARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
+
+#miCursor.execute("INSERT INTO PRODUCTOS VALUES('BALÓN', 15, 'DEPORTES')")
+
+variosProductos = [
+
+("Camiseta", 10, "Deportes"),
+("Jarrón", 90, "Cerámica"),
+("Caamión", 20, "Juguetería")
+
+]
+
+miCursor.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", variosProductos)
+
+
+miConexion.commit()
+
+miConexion.close()
