af7a87712001f2f2202c857ad1733cc925e4ac9f
Sebastián Cortés <sugusrojo@gmail.com>
2018-09-12 12:23:23 -0300

    Create Practica_lambda03.py

---
+++ pildorasinformaticas/clase_66/Practica_lambda03.py
@@ -0,0 +1,11 @@
+#al_cubo = lambda numero:numero**3
+#es lo mismo que 
+#al_cubo = lambda numero:pow(numero, 3)
+
+al_cubo = lambda numero:pow(numero, 3)
+
+print(al_cubo(13))
+
+al_cubo2 = lambda numero:numero**3
+
+print(al_cubo2(13))
